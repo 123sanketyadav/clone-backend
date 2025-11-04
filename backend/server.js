2e4af270d1886d0c427d2402c0f41ee715790276
Sankit Yadav <shivayadav886981@gmail.com>
2025-11-04 14:56:19 +0530

    Initial commit - Chat AI App

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import getGeminiAPIResponse from "./UTILS/openai.js";
 const app = express();
 const PORT =process.env.PORT || 8080;
 
-// ✅ Middleware
+// ✅ Middlewareuu
 app.use(cors({
   origin: "http://localhost:5173", 
   methods: ["GET", "POST", "PUT", "DELETE"],
